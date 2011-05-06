52e5cb5b3ac7672be3fd728b28d45a3df0f4afa0
Nicolas FRANCOIS <nicolas.franc@gmail.com>
2011-05-07 00:55:25 +0200

    create funkyjfunctionnal-fest-assert

---
+++ funkyjfunctional-fest-assert/src/main/java/info/piwai/funkyjfunctional/Cond.java
@@ -0,0 +1,11 @@
+package info.piwai.funkyjfunctional;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Nicolas
+ * Date: 07/05/11
+ * Time: 00:47
+ * To change this template use File | Settings | File Templates.
+ */
+public class Cond {
+}
