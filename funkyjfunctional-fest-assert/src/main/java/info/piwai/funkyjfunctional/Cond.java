88c969557084c9c04c324c8d7cd5d05ddd2c2f8c
Nicolas FRANCOIS <nicolas.franc@gmail.com>
2011-05-07 01:52:09 +0200

    Create Cond

--- funkyjfunctional-fest-assert/src/main/java/info/piwai/funkyjfunctional/Cond.java
+++ funkyjfunctional-fest-assert/src/main/java/info/piwai/funkyjfunctional/Cond.java
@@ -1,11 +1,54 @@
+/*
+ * Copyright 2011 Pierre-Yves Ricau (py.ricau at gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed To in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package info.piwai.funkyjfunctional;
 
+import info.piwai.funkyjfunctional.Funky;
+import org.fest.assertions.Condition;
+
 /**
- * Created by IntelliJ IDEA.
- * User: Nicolas
- * Date: 07/05/11
- * Time: 00:47
- * To change this template use File | Settings | File Templates.
+ * @author Nicolas Francois (nicolas.franc at gmail.com)
  */
-public class Cond {
+public abstract class Cond<T> {
+
+    /**
+     * <p>
+     * {@link ClassCondition} is not part of the API, which is why it has
+     * package-private scope.
+     */
+    static class ClassCondition<T, U extends Cond<T>> extends Condition<T> {
+
+        private final ClassExecutorWithInput<U> executor;
+
+        ClassCondition(ClassExecutorWithInput<U> executor) {
+            this.executor = executor;
+        }
+
+        @Override
+        public boolean matches(T input) {
+            U instance = executor.createExecutedInstance(input);
+            return instance.r;
+        }
+    }
+
+    protected T t;
+
+    protected boolean r;
+
+    public Cond() {
+        t = Funky.<T> getThreadLocalParameter();
+    }
+
 }
---
+++ funkyjfunctional-fest-assert/src/main/java/info/piwai/funkyjfunctional/FunkyFestAssert.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2011 Pierre-Yves Ricau (py.ricau at gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package info.piwai.funkyjfunctional;
+
+import org.fest.assertions.Condition;
+import info.piwai.funkyjfunctional.Cond.ClassCondition;
+import static info.piwai.funkyjfunctional.Funky.classExecutorWithInput;
+
+// TODO : docs
+public abstract class FunkyFestAssert {
+
+
+    public static <T, U extends Cond<T>> Condition<T> withCond(Class<U> applyingClass) {
+        return new ClassCondition<T, U>(classExecutorWithInput(applyingClass));
+    }
+
+    public static <T, U extends Cond<T>> Condition<T> withCond(Class<U> applyingClass, Object... constructorParameters) {
+        return new ClassCondition<T, U>(classExecutorWithInput(applyingClass, constructorParameters));
+    }
+
+
+    /**
+     * The constructor is private to make sure this class won't be instantiated.
+     * The class itself is abstract so that it even cannot be instantiated using
+     * the reflection API.
+     */
+    private FunkyFestAssert() {
+    }
+
+}
