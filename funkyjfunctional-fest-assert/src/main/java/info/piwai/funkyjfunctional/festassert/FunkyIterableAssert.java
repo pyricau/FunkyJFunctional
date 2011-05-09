d94769470d022443ca68ae2fb06ee26d0057296b
Pierre-Yves Ricau <py.ricau@gmail.com>
2011-05-09 17:31:20 +0200

    Found a better way to fix issue #26, using assertions instead of conditions. Closes issue #26 (BTW, thanks @nfrancois for the issue)

---
+++ funkyjfunctional-fest-assert/src/main/java/info/piwai/funkyjfunctional/festassert/FunkyIterableAssert.java
@@ -0,0 +1,85 @@
+/**
+ * Copyright (C) 2011 Pierre-Yves Ricau (py.ricau at gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed To in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package info.piwai.funkyjfunctional.festassert;
+
+import static info.piwai.funkyjfunctional.festassert.FunkyFestAssert.withCond;
+import static info.piwai.funkyjfunctional.festassert.NegativeCondition.not;
+
+import org.fest.assertions.Condition;
+import org.fest.assertions.IteratorAssert;
+
+/**
+ * @author Pierre-Yves Ricau (py.ricau at gmail.com)
+ */
+public class FunkyIterableAssert<T> extends IteratorAssert {
+    
+    public static <T> FunkyIterableAssert<T> assertThat(Iterable<T> actual) {
+        return new FunkyIterableAssert<T>(actual);
+    }
+
+    private final Iterable<T> actualIterable;
+
+    public FunkyIterableAssert(Iterable<T> actual) {
+        super(actual.iterator());
+        actualIterable = actual;
+    }
+    
+    public FunkyIterableAssert<T> eachSatisfies(Condition<T> condition) {
+        
+        long notMatching = 0;
+        StringBuilder notMatchingDescription = new StringBuilder();
+        long index = 0;
+        for (T value : actualIterable) {
+            if (!condition.matches(value)) {
+                notMatching++;
+                notMatchingDescription //
+                        .append("Element ") //
+                        .append(index) //
+                        .append(", [") //
+                        .append(value.toString()) //
+                        .append("] does not match condition: ") //
+                        .append(condition.description()) //
+                        .append("\n") //
+                ;
+            }
+            index++;
+        }
+
+        if (notMatching == 0) {
+            return this;
+        }
+
+        failIfCustomMessageIsSet();
+        throw failure(notMatching + " element(s) in the iterable not matching the provided condition:\n" + notMatchingDescription.toString());
+    }
+
+    public <U extends Cond<T>> FunkyIterableAssert<T> eachSatisfies(Class<U> applyingClass) {
+        eachSatisfies(withCond(applyingClass));
+        return this;
+    }
+
+    public <U extends Cond<T>> FunkyIterableAssert<T> noneSatisfies(Class<U> applyingClass) {
+        noneSatisfies(withCond(applyingClass));
+        return this;
+    }
+
+
+    public FunkyIterableAssert<T> noneSatisfies(Condition<T> condition) {
+        return eachSatisfies(not(condition));
+    }
+
+
+}
---
+++ funkyjfunctional-fest-assert/src/test/java/info/piwai/funkyjfunctional/festassert/IterableAssertTest.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright (C) 2011 Pierre-Yves Ricau (py.ricau at gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed To in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package info.piwai.funkyjfunctional.festassert;
+
+import static info.piwai.funkyjfunctional.festassert.FunkyIterableAssert.assertThat;
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class IterableAssertTest {
+
+    @Test
+    public void eachSatisfies() {
+       // @off
+       class Adult extends Cond<Integer> {{ r = t >= 18; }}
+       // @on
+
+       assertThat(Arrays.asList(19, 21, 69)).eachSatisfies(Adult.class);
+    }
+    
+    @Test
+    public void eachSatisfiesThrowsWhenOneDoesNot() {
+       // @off
+       class Adult extends Cond<Integer> {{ r = t >= 18; }}
+       // @on
+
+       try {
+           assertThat(Arrays.asList(17, 21, 69)).eachSatisfies(Adult.class);
+       } catch(AssertionError e) {
+           return;
+       }
+       fail("Assertion should have thrown an AssertionError");
+    }
+    
+    @Test
+    public void noneSatisfies() {
+       // @off
+       class Adult extends Cond<Integer> {{ r = t >= 18; }}
+       // @on
+
+       assertThat(Arrays.asList(14, -2, 17)).noneSatisfies(Adult.class);
+    }
+    
+    @Test
+    public void noneSatisfiesThrowsWhenOneDoes() {
+       // @off
+       class Adult extends Cond<Integer> {{ r = t >= 18; }}
+       // @on
+
+       try {
+           assertThat(Arrays.asList(14, -2, 18)).noneSatisfies(Adult.class);
+       } catch(AssertionError e) {
+           return;
+       }
+       fail("Assertion should have thrown an AssertionError");
+    }
+
+}
